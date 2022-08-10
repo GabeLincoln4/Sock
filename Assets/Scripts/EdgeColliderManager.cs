8e51779b8cc06fccd6822ef244d8b5a6cbbbae9b
Gavin <7797864896@K12.LCS>
2022-08-10 16:51:55 -0400

    Remove Mesh object and implementation

--- Assets/Scripts/EdgeColliderManager.cs
+++ Assets/Scripts/EdgeColliderManager.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class EdgeColliderManager : MonoBehaviour
 {
-    private Mesh colliderMesh;
-
     public void AdjustEdgeCollider2D(Vector2[] edges, GameObject gameObject, float x, float y)
     {
         gameObject.AddComponent<EdgeCollider2D>();
-        colliderMesh = gameObject.GetComponent<EdgeCollider2D>().CreateMesh(true, true);
         edges = gameObject.GetComponent<EdgeCollider2D>().points;
         edges[0] = new Vector2(x, y);
     }
