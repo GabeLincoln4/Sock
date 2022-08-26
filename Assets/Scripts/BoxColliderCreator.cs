e71d299aca5e343d94a645b360a0c74b55547b79
Gabriel Gavin <ggavin@outlook.com>
2022-08-26 18:47:51 -0400

    Create two box colliders that are the same size but have different offsets and return them to the sock manager

--- Assets/Scripts/BoxColliderCreator.cs
+++ Assets/Scripts/BoxColliderCreator.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class BoxColliderCreator : MonoBehaviour
 {
-    public BoxCollider2D CreateBoxCollider(GameObject gameObject)
+    public List<BoxCollider2D> CreateBoxCollider(GameObject gameObject)
     {
-        BoxCollider2D boxEdges;
-        float verticalColliderOffset = .15f;
+        List<BoxCollider2D> colliderList = new List<BoxCollider2D>();
+        BoxCollider2D topBox;
+        BoxCollider2D bottomBox;
+        float verticalColliderOffset = .135f;
         Vector2 newBoxSize = new Vector2(0.64f, 0.13f);
         Vector2 newBoxOffset = new Vector2(0f, verticalColliderOffset);
-        // Vector3 topBoxLocation = boxLocation.position;
-        // topBoxLocation.y = topBoxLocation.y + newBoxSize.x;
-        // Debug.Log(topBoxLocation);
-        // boxLocation.position = topBoxLocation;
+        Vector2 newBottomBoxOffset = new Vector2(0f, -verticalColliderOffset);
+        
+        topBox = gameObject.AddComponent<BoxCollider2D>();
+        bottomBox = gameObject.AddComponent<BoxCollider2D>();
+        Debug.Log(topBox.offset);
+        topBox.size = newBoxSize;
+        bottomBox.size = newBoxSize;
+        topBox.offset = newBoxOffset;
+        bottomBox.offset = newBottomBoxOffset;
+        Debug.Log(topBox.bounds.center.GetType());
+        colliderList.Add(bottomBox);
+        colliderList.Add(topBox);
 
-        boxEdges = gameObject.AddComponent<BoxCollider2D>();
-        Debug.Log(boxEdges.offset);
-        boxEdges.size = newBoxSize;
-        boxEdges.offset = newBoxOffset;
-        Debug.Log(boxEdges.bounds.center.GetType());
-
-        return boxEdges;
+        return colliderList;
     }
 }
