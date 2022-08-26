aa2ab58c61309eeb1d130f8cdee621ae4330a306
Gabriel Gavin <ggavin@outlook.com>
2022-08-26 17:27:08 -0400

    Create a vertical offset variable

--- Assets/Scripts/BoxColliderCreator.cs
+++ Assets/Scripts/BoxColliderCreator.cs
@@ -7,10 +7,19 @@ public class BoxColliderCreator : MonoBehaviour
     public BoxCollider2D CreateBoxCollider(GameObject gameObject)
     {
         BoxCollider2D boxEdges;
+        float verticalColliderOffset = .15f;
         Vector2 newBoxSize = new Vector2(0.64f, 0.13f);
+        Vector2 newBoxOffset = new Vector2(0f, verticalColliderOffset);
+        // Vector3 topBoxLocation = boxLocation.position;
+        // topBoxLocation.y = topBoxLocation.y + newBoxSize.x;
+        // Debug.Log(topBoxLocation);
+        // boxLocation.position = topBoxLocation;
 
         boxEdges = gameObject.AddComponent<BoxCollider2D>();
+        Debug.Log(boxEdges.offset);
         boxEdges.size = newBoxSize;
+        boxEdges.offset = newBoxOffset;
+        Debug.Log(boxEdges.bounds.center.GetType());
 
         return boxEdges;
     }
