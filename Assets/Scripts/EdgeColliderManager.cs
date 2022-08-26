14af7953158786306314002bb0df6d7d6dfcc18c
Gavin <7797864896@K12.LCS>
2022-08-26 14:16:29 -0400

    Turn on gizmos to show colliders at all times

--- Assets/Scripts/EdgeColliderManager.cs
+++ Assets/Scripts/EdgeColliderManager.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class EdgeColliderManager : MonoBehaviour
 {
-    public Vector2[] ModifyCollider(GameObject gameObject)
+    public EdgeCollider2D ModifyCollider(GameObject gameObject)
     {
-        Vector2[] edgeBorder;
+        EdgeCollider2D edgeBorder;
 
-        edgeBorder = gameObject.AddComponent<EdgeCollider2D>().points;
-        edgeBorder[0] = new Vector2(-.32f, 0f);
-        edgeBorder[1] = new Vector2(.18f, 0f);
+        edgeBorder = gameObject.AddComponent<EdgeCollider2D>();
         return edgeBorder;
     }
 }
