6ebb0c1c69a166d54bdd0bf5e52c43371ef55bbf
Gavin <7797864896@K12.LCS>
2022-09-17 12:03:16 -0400

    Add logic to make foot object move upward

--- Assets/Scripts/FootManager.cs
+++ Assets/Scripts/FootManager.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Manager : MonoBehaviour
+public class FootManager : MonoBehaviour
 {
     
-    [SerializeField] private GameObject foot;
+    [SerializeField] private GameObject _foot;
+    private Vector3 _direction = new Vector3(0, 1, 0);
+    [SerializeField] private float _movementSpeed;
     
     void Awake()
     {
-        foot.AddComponent<BoxCollider2D>();
+        _foot.AddComponent<BoxCollider2D>();
     }
 
     
     void Update()
     {
-        
+        _foot.transform.Translate(_direction * _movementSpeed * Time.deltaTime);
     }
 }
