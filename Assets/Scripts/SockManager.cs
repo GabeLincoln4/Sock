4ad997389635aa7c2a6168d0adb843cec99df87e
Gavin <7797864896@K12.LCS>
2022-09-25 23:23:44 -0400

    Remove code involving dynamic box colliders

--- Assets/Scripts/SockManager.cs
+++ Assets/Scripts/SockManager.cs
@@ -5,58 +5,19 @@ using UnityEngine;
 public class SockManager : MonoBehaviour
 {
     [SerializeField] private GameObject _sock;
-    [SerializeField] private string sockOpeningSize;
     [SerializeField] private bool _rotation;
     [SerializeField] private float _speed;
-    private Vector2[] _edges;
     private Transform _transform;
     private RotationResponse _rotationResponse;
     private EdgeColliderManager _colliderManager;
     private BoxColliderManager _boxCollider;
-    private ScalingResponse _scaleResponse;
-    private float _middleBoxHeight = .1f;
-    private Vector3 boundsOfSock;
     private Movement _movement;
-
 
     void Awake()
     {
-        boundsOfSock = _sock.GetComponent<Renderer>().bounds.size;
-        _colliderManager = _sock.AddComponent<EdgeColliderManager>();
         _movement = _sock.AddComponent<Movement>();
-        _boxCollider = _sock.AddComponent<BoxColliderManager>();
-
-        if (sockOpeningSize == "xl")
-        {
-            _boxCollider.CreateBoxCollider(_sock, .10f);
-            _boxCollider.CreateMiddleBoxCollider(_sock, _middleBoxHeight);
-        }
-
-        if (sockOpeningSize == "large")
-        {
-            _boxCollider.CreateBoxCollider(_sock, .11f);
-            _boxCollider.CreateMiddleBoxCollider(_sock, _middleBoxHeight);
-        }
-        
-        if (sockOpeningSize == "medium")
-        {
-            _boxCollider.CreateBoxCollider(_sock, .12f);
-            _boxCollider.CreateMiddleBoxCollider(_sock, _middleBoxHeight);
-        }
-
-        if (sockOpeningSize == "small")
-        {
-            Debug.Log("Size is small");
-        }
-
         _rotationResponse = _sock.AddComponent<RotationResponse>();
         _transform = _sock.GetComponent<Transform>();
-    }
-
-    void Start()
-    {
-        var sock = _sock;
-        var edges = _edges;
     }
 
     void Update()
